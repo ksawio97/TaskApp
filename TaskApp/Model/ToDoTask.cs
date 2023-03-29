b57b02414e52d0c80a94ec9b29c3ba7494529247
ksawio97 <ksawery9708@gmail.com>
2023-03-29 17:45:52 +0200

    initial commit

---
+++ TaskApp/Model/ToDoTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Model
+{
+    public class ToDoTask
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
